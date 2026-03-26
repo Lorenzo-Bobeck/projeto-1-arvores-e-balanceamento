b4e4621ef181076a387f406b80c5de9aeccaed53
Lorenzo <lorenzobobeck4535@gmail.com>
2026-03-26 16:38:42 -0300

    Última parte da Árvore BST

--- src/trees/arvore bst.ts
+++ src/trees/arvore bst.ts
@@ -41,4 +41,29 @@ export class BST {
     return 1 + Math.max(this.height(node.left), this.height(node.right));
   }
 
+  remove(key: number) {
+    this.root = this._remove(this.root, key);
+  }
+
+  private _remove(node: Node | null, key: number): Node | null {
+    if (!node) return null;
+
+    if (key < node.key) node.left = this._remove(node.left, key);
+    else if (key > node.key) node.right = this._remove(node.right, key);
+    else {
+      if (!node.left) return node.right;
+      if (!node.right) return node.left;
+
+      let min = this.findMin(node.right);
+      node.key = min.key;
+      node.right = this._remove(node.right, min.key);
+    }
+
+    return node;
+  }
+
+  private findMin(node: Node): Node {
+    while (node.left) node = node.left;
+    return node;
+  }
 }
